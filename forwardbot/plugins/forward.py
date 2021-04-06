39ea34a235ea828d2852a9f9c7ad8ac5ae88e49b
subinps <64341611+subinps@users.noreply.github.com>
2021-04-06 23:44:27 +0530

    Update forward.py

--- forwardbot/plugins/forward.py
+++ forwardbot/plugins/forward.py
@@ -234,7 +234,7 @@ async def handler(event):
                     if mcount:
                         if media_type(message) == 'Document':
                             try:
-                                await client.send_file(tochat, message.document ,caption="@subinps") 
+                                await client.send_file(tochat, message.document ,caption=f"A PROJECT BY XTZ BOTS \n\n ©@subinps") 
                                 try:
                                   if len(str(message.file.name)) <= 95:
                                     print("Succesfully forwarded: " + str(message.file.name))
